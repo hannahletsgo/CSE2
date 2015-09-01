3f7cf8f3bcd18376e4afbb782c4f285c5e7dc314
hannahletsgo <hcl218@lehigh.edu>
2015-09-01 13:34:22 +0000

    updated WelcomeClass

---
+++ hw01-WelcomeClass/WelcomeClass.java
@@ -0,0 +1,34 @@
+//Hannah Leskow
+//HW 1
+//Welcome Class Program
+
+//first compile the program
+//  javac WelcomeClass.java
+//then run the program
+// java WelcomeClass//
+
+//define a class
+public class WelcomeClass{
+    
+    //define a main method
+    public static void main(String[] args){
+        
+        //  Display output welcome screen with lehigh id and 
+        //  autobiographical statement
+        
+        System.out.println("  -----------");
+        System.out.println("  | WELCOME |");
+        System.out.println("  -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        // Must use 2 // to avoid illegal escape error /
+        String s = " / \\/ \\/ \\/ \\/ \\/ \\";
+        System.out.println(s);
+        System.out.println("|-H--C--L--2--1--8-|");
+        String t = " \\ /\\ /\\ /\\ /\\ /\\ /";
+        System.out.println(t);
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("I enjoy long walks on the beach and chocolate ice cream.");
+    
+        //test
+    }
+}
